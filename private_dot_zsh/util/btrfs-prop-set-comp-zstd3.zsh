61d0750481d7aef665fec87a48c6d5dd75a7947b
0xTadash1 <56265995+0xTadash1@users.noreply.github.com>
2022-12-09 04:46:38 +0900

    move `XDG_xxx_HOME`s root `~` to `~/xdg`, and many

--- private_dot_zsh/util/btrfs-prop-set-comp-zstd3.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-btrfs property set $HOME/Downloads compression zstd:3
-btrfs property set $HOME/Pictures  compression zstd:3
-btrfs property set $HOME/Videos    compression zstd:3
-btrfs property set $HOME/Music     compression zstd:3
